1b3310275fd04e94c6a96611421ea86b85df4033
CaroEcc <ecc5000676@est.univalle.edu>
2021-10-11 10:13:58 -0400

    Task9

--- Scire.Univalle/js/loginScript.js
+++ Scire.Univalle/js/loginScript.js
@@ -21,6 +21,18 @@ window.onload = function () {
         });
     }
 }
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
+
 
 
 /* End of JS to implement panel  */
